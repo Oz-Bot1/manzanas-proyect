8c210b9c4c4c61a96ab9a184aeef95fc7e74baf8
Oz <oscari24155@gmail.com>
2023-09-28 10:12:59 -0600

    Seguridad en admin

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,16 +1,18 @@
 import { CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { LoginService } from './service/login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private loginService: LoginService, private router: Router) {}
+  constructor(private loginService: LoginService, private router: Router, private cookie: CookieService) {}
 
   canActivate(): boolean {
-    if (this.loginService.isLoggedIn()) {
+    const rol = +this.cookie.get('idRol');
+    if (this.loginService.isLoggedIn() && rol === 1) {
       return true;
     } else {
       this.router.navigate(['/login']);
