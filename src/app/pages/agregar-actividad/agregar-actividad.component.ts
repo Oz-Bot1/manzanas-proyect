bbc1f3f391b15ecaac4f27d006a0f10004b028e9
Oz <oscari24155@gmail.com>
2023-09-30 17:16:09 -0600

    Agregar actividad

--- src/app/pages/agregar-actividad/agregar-actividad.component.ts
+++ src/app/pages/agregar-actividad/agregar-actividad.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AgregarService } from 'src/app/service/agregar.service';
 import { LoginService } from 'src/app/service/login.service';
 
 @Component({
@@ -11,8 +12,7 @@ import { LoginService } from 'src/app/service/login.service';
 export class AgregarActividadComponent {
   actividadForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
-    // Define tu formulario reactivo con los campos necesarios y las validaciones
+  constructor(private fb: FormBuilder, private router: Router, private agregarService: AgregarService) {
     this.actividadForm = this.fb.group({
       nombre: ['', Validators.required],
       foto: [''],
@@ -20,10 +20,45 @@ export class AgregarActividadComponent {
     });
   }
 
-  onSubmit() {
-    console.log(this.actividadForm.value);
+  obj: any = {};
+  nombrefoto: string = '';
+  onFileSelect(input: any) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.obj.photoUrl = e.target.result;
+        this.agregarService.saveImage(this.obj.photoUrl).subscribe({
+          next: (data) => {
+            this.nombrefoto = data.fileName.nombre;
+            console.log(this.nombrefoto);
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        });
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
   }
-  
+  onSubmit() {
+    if (this.actividadForm.valid) {
+      const nombre = this.actividadForm.get('nombre')?.value;
+      const descripcion = this.actividadForm.get('descripcion')?.value;
+      const fotoControl = this.nombrefoto;
+
+      this.agregarService.registrarActividad(nombre, descripcion, fotoControl).subscribe({
+        next: () => {
+          this.router.navigate(['/actividadesAdmin']);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    } else {
+      console.log('Complete el formulario');
+    }
+  }
+
   liberar() {
     this.router.navigate(['/liberar']);
   }
