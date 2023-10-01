a23cad669f3a25405648650730a48e30265396fb
Oz <oscari24155@gmail.com>
2023-10-01 14:42:15 -0600

    Eliminacion de metodos aun no utilizados

--- src/app/service/actividades.service.ts
+++ src/app/service/actividades.service.ts
@@ -16,30 +16,4 @@ export class ActividadesService {
     return this.http.post<any>(`${urlApi}/Dashboard/listaActividades`, params);
   }
 
-  buscar(empresa: string, fechaInicial: string, fechaFinal: string): Observable<any> {
-    const params = {
-      empresa: empresa,
-      fechaInicial: fechaInicial,
-      fechaFinal: fechaFinal
-    };
-    return this.http.post<any>(`${urlApi}/Actividad/buscar`, params);
-  }
-
-  actualizar(empresa: string, fechaInicial: string, fechaFinal: string): Observable<any> {
-    const params = {
-      empresa: empresa,
-      fechaInicial: fechaInicial,
-      fechaFinal: fechaFinal
-    };
-    return this.http.post<any>(`${urlApi}/Actividad/actualizar`, params);
-  }
-
-  eliminar(empresa: string, fechaInicial: string, fechaFinal: string): Observable<any> {
-    const params = {
-      empresa: empresa,
-      fechaInicial: fechaInicial,
-      fechaFinal: fechaFinal
-    };
-    return this.http.post<any>(`${urlApi}/Actividad/eliminar`, params);
-  }
 }
