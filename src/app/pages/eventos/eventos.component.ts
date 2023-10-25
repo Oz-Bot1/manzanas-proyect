b1c59bdd707b5018bba942d32bfc5c8484b874d6
Oz <oscari24155@gmail.com>
2023-10-25 10:28:34 -0600

    Modficacion de eventos para redirigir a agregar

--- src/app/pages/eventos/eventos.component.ts
+++ src/app/pages/eventos/eventos.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AgregarService } from 'src/app/service/agregar.service';
 import { EventosService } from 'src/app/service/eventos.service';
-import * as L from 'leaflet';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 @Component({
@@ -15,20 +13,15 @@ export class EventosComponent implements OnInit {
   puntoVentaForm: FormGroup;
   latitud: number = 0;
   longitud: number = 0;
-
   listaEventos: any[] = [];
+  idAct: number = 0;
+  nombreProducto: string = '';
 
-  constructor(private fb: FormBuilder, private eventosService: EventosService, private agregarService: AgregarService, private router: Router) {
+  constructor(private fb: FormBuilder, private eventosService: EventosService, private router: Router) {
     this.puntoVentaForm = this.fb.group({
       nombre: ['', Validators.required],
       estatus: ['1'], // Valor por defecto 'Activo'
       horario: ['', Validators.required],
-    });
-    this.eventoForm = this.fb.group({
-      nombre: ['', Validators.required],
-      fechaInicio: ['', Validators.required],
-      fechaFin: ['', Validators.required],
-      descripcion: ['', Validators.required],
     });
   }
 
@@ -56,95 +49,30 @@ export class EventosComponent implements OnInit {
     return nombre;
   }
 
-  idAct: number = 0;
-  nombreProducto: string = '';
-  buscarPunto(id: number) {
+  buscarPunto(id: number, rol: number) {
     this.eventosService.buscarPunto(id).subscribe(
       {
         next: (data) => {
           this.idAct = data.data[0].id;
-          this.nombreProducto = data.data[0].nombre;
-          const actividad = data.data[0];
-          this.nombrefoto = actividad.foto;
-          this.puntoVentaForm.patchValue({
-            nombre: actividad.nombre,
-            foto: actividad.foto,
-            estatus: actividad.estatus,
-            horario: actividad.horario,
-          });
-          this.latitud = actividad.latitud;
-          this.longitud = actividad.longitud;
+          if (rol === 1) {
+            localStorage.setItem('idAct', this.idAct.toString());
+            this.router.navigate(['/agregarPunto']);
+          } else {
+            this.nombreProducto = data.data[0].nombre;
+          }
         },
         error: (error) => {
           console.log(error);
-        },
-        complete: () => {
-          const map = L.map('map').setView([this.latitud, this.longitud], 15);
-
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          }).addTo(map);
-
-          // Crear un icono personalizado
-          const customIcon = L.icon({
-            iconUrl: '../../../assets/img/marcador.png',
-            iconSize: [50, 50], // Tamaño del icono [ancho, alto]
-            iconAnchor: [16, 32], // Punto de anclaje del icono [horizontal, vertical]
-          });
-
-          const marker = L.marker([this.latitud, this.longitud], { icon: customIcon })
-            .bindPopup('¡Aqui!')
-            .openPopup();
-
-          marker.addTo(map);
-
-          // Configurar evento de clic en el mapa
-          map.on('click', (e) => {
-            // Obtener las coordenadas donde se hizo clic
-            const latLng = e.latlng;
-
-            // Mover el marcador a las coordenadas donde se hizo clic
-            marker.setLatLng(latLng);
-
-            this.latitud = this.eliminarUltimosDigitos(latLng.lat, 5);
-            this.longitud = this.eliminarUltimosDigitos(latLng.lng, 5);
-          });
         }
       }
     );
-  }
-
-  eliminarUltimosDigitos(coordenada: number, cantidadDigitos: number): number {
-    const factor = Math.pow(10, cantidadDigitos);
-    return Math.floor(coordenada * factor) / factor;
-  }
-
-  obj: any = {};
-  nombrefoto: string = '';
-  onFileSelect(input: any) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.obj.photoUrl = e.target.result;
-        this.agregarService.saveImage(this.obj.photoUrl).subscribe({
-          next: (data) => {
-            this.nombrefoto = data.fileName.nombre;
-            console.log(this.nombrefoto);
-          },
-          error: (error) => {
-            console.log(error);
-          }
-        });
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
   }
 
   actualizarPunto() {
     if (this.puntoVentaForm.valid) {
       const id = this.idAct;
       const nombre = this.puntoVentaForm.get('nombre')?.value;
-      const fotoControl = this.nombrefoto;
+      const fotoControl = '';
       const latitud = this.latitud.toString();
       const longitud = this.longitud.toString();
       const estatus = this.puntoVentaForm.get('estatus')?.value;
@@ -175,66 +103,22 @@ export class EventosComponent implements OnInit {
     });
   }
 
-  buscarEvento(id: number) {
+  buscarEvento(id: number, rol: number) {
     this.eventosService.buscarEvento(id).subscribe(
       {
         next: (data) => {
           this.idAct = data.data[0].id;
-          localStorage.setItem('idAct', this.idAct.toString());
-          this.router.navigate(['/agregarEvento']);
-           this.nombreProducto = data.data[0].nombre;
-          // const actividad = data.data[0];
-          // this.nombrefoto = actividad.foto;
-          // this.eventoForm.patchValue({
-          //   nombre: actividad.nombre,
-          //   foto: actividad.foto,
-          //   descripcion: actividad.descripcion,
-          //   fechaInicio: actividad.fechaInicio,
-          //   fechaFin: actividad.fechaFin
-          // });
-          // this.latitud = this.eliminarUltimosDigitos(actividad.latitud, 5);
-          // this.longitud = this.eliminarUltimosDigitos(actividad.longitud, 5);
-        },
-        error: (error) => {
-          console.log(error);
-        },
-        complete: () => {
-          const map = L.map('mapEvento').setView([this.latitud, this.longitud], 15);
-
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          }).addTo(map);
-
-          // Crear un icono personalizado
-          const customIcon = L.icon({
-            iconUrl: '../../../assets/img/marcador.png',
-            iconSize: [50, 50], // Tamaño del icono [ancho, alto]
-            iconAnchor: [16, 32], // Punto de anclaje del icono [horizontal, vertical]
-          });
-
-          const marker = L.marker([this.latitud, this.longitud], { icon: customIcon })
-            .bindPopup('¡Aqui!')
-            .openPopup();
-
-          marker.addTo(map);
-
-          // Configurar evento de clic en el mapa
-          map.on('click', (e) => {
-            // Obtener las coordenadas donde se hizo clic
-            const latLng = e.latlng;
-
-            // Mover el marcador a las coordenadas donde se hizo clic
-            marker.setLatLng(latLng);
-
-            this.latitud = this.eliminarUltimosDigitos(latLng.lat, 5);
-            this.longitud = this.eliminarUltimosDigitos(latLng.lng, 5);
-          });
+          if (rol === 1) {
+            localStorage.setItem('idAct', this.idAct.toString());
+            this.router.navigate(['/agregarEvento']);
+          } else {
+            this.nombreProducto = data.data[0].nombre;
+          }
         }
       }
     );
   }
 
-  eventoForm: FormGroup;
   eliminarEvento() {
     const id = this.idAct;
     this.eventosService.eliminarEvento(id).subscribe({
@@ -245,29 +129,6 @@ export class EventosComponent implements OnInit {
         console.log(error);
       }
     });
-  }
-
-  actualizarEvento() {
-    if (this.eventoForm.valid) {
-      const id = this.idAct.toString();
-      const nombre = this.eventoForm.get('nombre')?.value;
-      const descripcion = this.eventoForm.get('descripcion')?.value;
-      const fechaInicio = this.eventoForm.get('fechaInicio')?.value;
-      const fechaFin = this.eventoForm.get('fechaFin')?.value;
-      const latitud = this.latitud.toString();
-      const longitud = this.longitud.toString();
-      const foto = this.nombrefoto;
-      this.eventosService.actualizarEvento(id, nombre, foto, latitud, longitud, descripcion, fechaInicio, fechaFin).subscribe({
-        next: () => {
-          location.reload();
-        },
-        error: (error) => {
-          console.log(error);
-        }
-      });
-    } else {
-      alert('invalido');
-    }
   }
 
 }
