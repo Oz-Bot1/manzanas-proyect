5abb4d2818e37853e877410428c93a7ceece9a19
Oz <oscari24155@gmail.com>
2023-10-25 09:55:52 -0600

    Modificacion de agregar para actualizar evento

--- src/app/pages/agregar-evento/agregar-evento.component.ts
+++ src/app/pages/agregar-evento/agregar-evento.component.ts
@@ -12,6 +12,7 @@ import { EventosService } from 'src/app/service/eventos.service';
 })
 export class AgregarEventoComponent implements OnInit, OnDestroy {
   titulo: string = 'Agregar Evento';
+  tituloBoton: string = 'Agregar Evento';
   eventoForm: FormGroup;
 
   constructor(private fb: FormBuilder, private router: Router, private agregarService: AgregarService, private eventosService: EventosService) {
@@ -26,7 +27,6 @@ export class AgregarEventoComponent implements OnInit, OnDestroy {
     localStorage.clear();
   }
 
-  nombreProducto: string = '';
   idAct: number = 0;
 
   buscarEvento(id: number) {
@@ -34,7 +34,6 @@ export class AgregarEventoComponent implements OnInit, OnDestroy {
       {
         next: (data) => {
           this.idAct = data.data[0].id;
-          this.nombreProducto = data.data[0].nombre;
           const actividad = data.data[0];
           this.nombrefoto = actividad.foto;
           this.eventoForm.patchValue({
@@ -52,85 +51,68 @@ export class AgregarEventoComponent implements OnInit, OnDestroy {
           console.log(error);
         },
         complete: () => {
-          const map = L.map('map').setView([this.latitud, this.longitud], 15);
-
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          }).addTo(map);
-
-          // Crear un icono personalizado
-          const customIcon = L.icon({
-            iconUrl: '../../../assets/img/marcador.png',
-            iconSize: [50, 50], // Tamaño del icono [ancho, alto]
-            iconAnchor: [16, 32], // Punto de anclaje del icono [horizontal, vertical]
-          });
-
-          const marker = L.marker([this.latitud, this.longitud], { icon: customIcon })
-            .bindPopup('¡Aqui!')
-            .openPopup();
-
-          marker.addTo(map);
-
-          // Configurar evento de clic en el mapa
-          map.on('click', (e) => {
-            // Obtener las coordenadas donde se hizo clic
-            const latLng = e.latlng;
-
-            // Mover el marcador a las coordenadas donde se hizo clic
-            marker.setLatLng(latLng);
-
-            this.latitud = this.eliminarUltimosDigitos(latLng.lat, 5);
-            this.longitud = this.eliminarUltimosDigitos(latLng.lng, 5);
-          });
+          this.generarMapa(this.latitud, this.longitud);
         }
       }
     );
   }
 
+  obtenerNombreImagen(nombre: string): string {
+    if (nombre.endsWith('.jpeg')) {
+      return nombre.slice(0, -5); // Elimina los últimos 5 caracteres (".jpeg")
+    }
+    return nombre;
+  }
+
   latitud: number = 19.7760972227;
   longitud: number = -97.385921503;
+  id = localStorage.getItem('idAct');
   ngOnInit(): void {
-    const id = localStorage.getItem('idAct');
-    if (id !== null) {
-      const idAsNumber = parseInt(id, 10);
+    if (this.id !== null) {
+      const idAsNumber = parseInt(this.id, 10);
       if (!isNaN(idAsNumber)) {
+        this.titulo = "Actualizar Evento";
+        this.tituloBoton = 'Actualizar Evento'
         this.buscarEvento(idAsNumber);
       } else {
-        // 'id' no es un número válido
+        this.router.navigate(['/eventos']);
       }
     } else {
-      const map = L.map('map').setView([this.latitud, this.longitud], 15);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      // Crear un icono personalizado
-      const customIcon = L.icon({
-        iconUrl: '../../../assets/img/marcador.png',
-        iconSize: [50, 50], // Tamaño del icono [ancho, alto]
-        iconAnchor: [16, 32], // Punto de anclaje del icono [horizontal, vertical]
-      });
-
-      const marker = L.marker([this.latitud, this.longitud], { icon: customIcon })
-        .bindPopup('¡Aqui!')
-        .openPopup();
-
-      marker.addTo(map);
-
-      // Configurar evento de clic en el mapa
-      map.on('click', (e) => {
-        // Obtener las coordenadas donde se hizo clic
-        const latLng = e.latlng;
-
-        // Mover el marcador a las coordenadas donde se hizo clic
-        marker.setLatLng(latLng);
-
-        this.latitud = this.eliminarUltimosDigitos(latLng.lat, 5);
-        this.longitud = this.eliminarUltimosDigitos(latLng.lng, 5);
-      });
+      this.generarMapa(this.latitud, this.longitud);
     }
+  }
 
+  generarMapa(latitud: number, longitud: number) {
+    const map = L.map('map').setView([latitud, longitud], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    // Crear un icono personalizado
+    const customIcon = L.icon({
+      iconUrl: '../../../assets/img/marcador.png',
+      iconSize: [50, 50], // Tamaño del icono [ancho, alto]
+      iconAnchor: [16, 32], // Punto de anclaje del icono [horizontal, vertical]
+    });
+
+    const marker = L.marker([latitud, longitud], { icon: customIcon })
+      .bindPopup('¡Aqui!')
+      .openPopup();
+
+    marker.addTo(map);
+
+    // Configurar evento de clic en el mapa
+    map.on('click', (e) => {
+      // Obtener las coordenadas donde se hizo clic
+      const latLng = e.latlng;
+
+      // Mover el marcador a las coordenadas donde se hizo clic
+      marker.setLatLng(latLng);
+
+      this.latitud = this.eliminarUltimosDigitos(latLng.lat, 5);
+      this.longitud = this.eliminarUltimosDigitos(latLng.lng, 5);
+    });
   }
 
   eliminarUltimosDigitos(coordenada: number, cantidadDigitos: number): number {
@@ -169,14 +151,30 @@ export class AgregarEventoComponent implements OnInit, OnDestroy {
       const longitud = this.longitud.toString();
       const foto = this.nombrefoto;
 
-      this.agregarService.registrarEvento(nombre, descripcion, fechaInicio, fechaFin, latitud, longitud, foto).subscribe({
-        next: () => {
+      if (this.id !== null) {
+        const idAsNumber = parseInt(this.id, 10);
+        if (!isNaN(idAsNumber)) {
+          this.eventosService.actualizarEvento(this.id, nombre, foto, latitud, longitud, descripcion, fechaInicio, fechaFin).subscribe({
+            next: () => {
+              location.reload();
+            },
+            error: (error) => {
+              console.log(error);
+            }
+          });
+        } else {
           this.router.navigate(['/eventos']);
-        },
-        error: (error) => {
-          console.log(error);
         }
-      });
+      } else {
+        this.agregarService.registrarEvento(nombre, descripcion, fechaInicio, fechaFin, latitud, longitud, foto).subscribe({
+          next: () => {
+            this.router.navigate(['/eventos']);
+          },
+          error: (error) => {
+            console.log(error);
+          }
+        });
+      }
     } else {
       console.log('Complete el formulario');
     }
